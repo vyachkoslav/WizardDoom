01ba0d5f383929d2a9e08c250628db41feacaff6
heikki-heinonen <heikki.heinonen@edu.turkuamk.fi>
2025-10-01 13:33:45 +0300

    Added ranged enemy attack range
    Ranged enemy:
    - moves to attack range if it detects the player and isn't in attack range yet
    - moves to the outer boundary of attack range if player gets too close

--- Assets/Scripts/Enemy/FollowPlayer.cs
+++ Assets/Scripts/Enemy/FollowPlayer.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 using UnityEngine.AI;
 
 
-//To be added later: 
-//-fleeing behaviour only happens when player is very close to enemy
-//- ranged enemy moving to attack range to be added later
+//TO BE ADDED LATER: 
+// - split this script in two, melee and ranged version
+//  * create a separate RangedEnemyBehaviour script, use global variables, define methods for fleeing etc
+// - cleanup
 
 public class FollowPlayer : MonoBehaviour
 {
@@ -13,7 +14,9 @@ public class FollowPlayer : MonoBehaviour
     private Transform target;
     private string selfType;
     private Vector3 lastKnownPlayerPosition;
+    private float distanceToPlayer;
     private float rotationSpeed = 240f;
+    private bool IsFleeing = false;
 
     void Start()
     {
@@ -45,25 +48,58 @@ public class FollowPlayer : MonoBehaviour
 
     void RangedBehaviour()
     {
-        float fleeDistance = 2f;
+        float fleeTriggerRange = 2f;
+        float fleeDistance = GetComponentInChildren<AttackRange>().rangeSize - fleeTriggerRange;
+        bool IsInAttackRange = GetComponentInChildren<AttackRange>().IsInAttackRange;
 
         if (GetComponent<DetectPlayer>().playerIsDetected)
         {
             target = player.transform;
             lastKnownPlayerPosition = target.position;
 
-            // Makes enemy flee directly away from player by a fixed distance
-            Vector3 directionAwayFromPlayer = -(target.position - transform.position).normalized;
-            Vector3 fleePosition = transform.position + directionAwayFromPlayer * fleeDistance;
-            agent.SetDestination(fleePosition);
+            distanceToPlayer = (target.position - transform.position).magnitude;
+            Debug.Log("distance to player: " + distanceToPlayer);
+
+            if (IsInAttackRange)
+            {
+                if (distanceToPlayer < fleeTriggerRange)
+                {
+                    // Makes enemy flee directly away from player by a fixed distance
+                    Vector3 directionAwayFromPlayer = -(target.position - transform.position).normalized;
+                    Vector3 fleePosition = transform.position + directionAwayFromPlayer * fleeDistance;
+                    agent.isStopped = false; // remove when attack implemented
+                    IsFleeing = true;
+                    agent.SetDestination(fleePosition);
+                    Debug.Log("fleeing");
+                }
+                else if (!IsFleeing)
+                {
+                    LookAtPlayer();
+                    // Insert attack here
+                    agent.isStopped = true; // placeholder
+                    Debug.Log("Attack");
+                }
+                    
+            }
+            else if (!IsInAttackRange && !IsFleeing)
+            {
+                agent.isStopped = false; // remove when attack implemented
+                agent.SetDestination(target.position);
+                Debug.Log("moving to attack range");
+            }
         }
         else if (CheckIfDestinationReached())
         {
-            // Rotates enemy to look at the last known player location
-            Vector3 directionToLook = lastKnownPlayerPosition - transform.position;
-            Debug.DrawLine(transform.position, directionToLook, Color.yellow);
-            Quaternion targetRotation = Quaternion.LookRotation(directionToLook);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            LookAtPlayer();
+        }
+
+        if (IsFleeing)
+        {
+            if (CheckIfDestinationReached())
+            {
+                IsFleeing = false;
+                LookAtPlayer();
+            }
         }
     }
 
@@ -80,10 +116,20 @@ public class FollowPlayer : MonoBehaviour
                 {
                     //Debug.Log("Destination reached");
                     destinationReached = true;
+                    IsFleeing = false;
                 }
             }
         }
 
         return destinationReached;
+    }
+
+    void LookAtPlayer()
+    {
+        // Rotates enemy to look at the last known player location
+        Vector3 directionToLook = lastKnownPlayerPosition - transform.position;
+        Debug.DrawLine(transform.position, directionToLook, Color.yellow);
+        Quaternion targetRotation = Quaternion.LookRotation(directionToLook);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
