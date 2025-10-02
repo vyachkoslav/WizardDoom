3675be2542c82cb0df5cd8b1f230939025d11684
ElDiamondo <utile.peach2@gmail.com>
2025-10-02 20:00:36 +0300

    Fixed so the PauseMenu doesn't CharacterControls
    Problem still with disable method as the game doesn't  show the mouse.

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -6,34 +6,61 @@ using UnityEngine.Pool;
 public class PauseMenu : MonoBehaviour
 {
 
+    private bool isPaused;
+
     public GameObject pauseMenu;
     public Player.CharacterControls Player;
+    public Player.WeaponController Weapon;
     [SerializeField] private InputActionReference pauseAction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        isPaused = false;
         pauseMenu.SetActive(false);
     }
 
     void OnEnable()
     {
-        pauseAction.action.performed += PauseGame;
-        pauseAction.action.canceled += ResumeGame;
-        
+        pauseAction.action.performed += TogglePause;
     }
 
-    public void PauseGame(InputAction.CallbackContext context)
+    public void TogglePause(InputAction.CallbackContext context)
     {
-        Player.TogglePause();
+        if (pauseMenu.activeSelf)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+
+    public void PauseGame()
+    {
+        isPaused = true;
+        Player.enabled = false;
+        Weapon.enabled = false;
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
     }
 
-    public void ResumeGame(InputAction.CallbackContext context)
+    public void ResumeGame()
     {
-        Player.TogglePause();
+        isPaused = false;
+        Player.enabled = true;
+        Weapon.enabled = true;
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
     }
+
+
+
+    public void OnResumeButtonCliked()
+    {
+        ResumeGame();//TODO add this to the resume game button
+    }
 }
+
--- Assets/Scripts/Player/CharacterControls.cs
+++ Assets/Scripts/Player/CharacterControls.cs
@@ -1,12 +1,11 @@
-using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Player
 {
     public class CharacterControls : MonoBehaviour
-    {
 
+    {
         [SerializeField] private CharacterController characterController;
         [SerializeField] private Transform cameraTransform;
 
@@ -14,38 +13,34 @@ namespace Player
         [SerializeField] private InputActionReference movementAction;
         [SerializeField] private InputActionReference lookAction;
 
+
         [Header("Settings")]
         [SerializeField] private float maxVerticalLookAngle;
         [SerializeField] private float lookSensitivity;
         [SerializeField] private float speed;
 
+
         private Vector3 direction;
         private float verticalLook;
 
-        //Is used when pausing the game
-        private bool isPaused;
-        //The game now checks if the pause menu is enabled and dissallows camera movement if it is
+
         public void AddCameraDelta(Vector2 delta)
-        { 
-            if (!isPaused)
-            {
-                characterController.transform.Rotate(new Vector3() { y = delta.x });
+        {
+            characterController.transform.Rotate(new Vector3() { y = delta.x });
 
-                verticalLook = verticalLook + -delta.y;
-                verticalLook = Mathf.Clamp(verticalLook, -maxVerticalLookAngle, maxVerticalLookAngle);
 
-                var camEulers = cameraTransform.localEulerAngles;
-                camEulers.x = verticalLook;
-                cameraTransform.localEulerAngles = camEulers;
-            }
-
+            verticalLook = verticalLook + -delta.y;
+            verticalLook = Mathf.Clamp(verticalLook, -maxVerticalLookAngle, maxVerticalLookAngle);
+            var camEulers = cameraTransform.localEulerAngles;
+            camEulers.x = verticalLook;
+            cameraTransform.localEulerAngles = camEulers;
         }
+
 
         private void Awake()
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
-            isPaused = false;
         }
 
         private void OnEnable()
@@ -67,54 +62,26 @@ namespace Player
         }
 
         private void OnLook(InputAction.CallbackContext ctx)
+
         {
             var delta = ctx.ReadValue<Vector2>();
             delta *= lookSensitivity;
+
             AddCameraDelta(delta);
-        }
 
-
-        //Switches the pause state(from PauseMenu script)
-        public void TogglePause()
-        {
-            isPaused = !isPaused;
-        }
-
-        //Shows the cursor it the game is paused and hides it if the game is not paused
-        private void ToggleCursor()
-        {
-            if (isPaused)
-            {
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
-            else
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
-
-        }
-
-        //Checks if the game is paused or not and allows movement
-        private void Movement()
-        {
-            if (!isPaused)
-            {
-                var forward = characterController.transform.forward;
-                var right = characterController.transform.right;
-                var moveAmount = Time.deltaTime * speed;
-                var movementY = (direction.y * moveAmount) * forward;
-                var movementX = (direction.x * moveAmount) * right;
-                var movement = movementX + movementY;
-                characterController.Move(movement);
-            }
         }
 
         private void Update()
         {
-            Movement();
-            ToggleCursor();
+            var forward = characterController.transform.forward;
+            var right = characterController.transform.right;
+            var moveAmount = Time.deltaTime * speed;
+            var movementY = (direction.y * moveAmount) * forward;
+            var movementX = (direction.x * moveAmount) * right;
+            var movement = movementX + movementY;
+            characterController.Move(movement);
         }
+
     }
+
 }
