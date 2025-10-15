49cbaca4eb834f4acd85af4138f80bc94ae5b674
ElDiamondo <utile.peach2@gmail.com>
2025-10-15 14:34:28 +0300

    Added main menu, sound management still missing

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //Load the first level here for now the SampleScene is used
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    //TODO when audio manager is done add the code for managing sound sliders here
+}
