b96de696d625114a8a2f3850b820d564c822fd06
tyomies123 <46247024+tyomies123@users.noreply.github.com>
2025-10-28 13:36:21 +0200

    Implemented mana regeneration
    - Mana regen per second, adjusted in the inspector

--- Assets/Scripts/Player/SpellController.cs
+++ Assets/Scripts/Player/SpellController.cs
@@ -1,7 +1,8 @@
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Player.Spells;
+using System.Collections.Generic;
 
 namespace Player
 {
@@ -19,11 +20,15 @@ namespace Player
         [SerializeField] private List<Spell> _spellList;
 
         [Header("Mana")]
-        [SerializeField] private int _playerMana;
+        [SerializeField] private int _maxMana;
+        [SerializeField] private int _manaRegenPerSecond;
 
         private int _currentSpellIndex;
         private Spell _currentSelectedSpell;
+        private int _currentMana;
         private int _manaCost;
+
+        private bool _manaRegen = false;
 
         public Transform ProjectileSpawn { get { return _projectileSpawn; }}
 
@@ -31,6 +36,7 @@ namespace Player
         private void Awake()
         {
             _currentSpellIndex = 0;
+            _currentMana = _maxMana;
             Debug.Log(_spellList.Count);
         }
 
@@ -53,15 +59,15 @@ namespace Player
             _currentSelectedSpell = _spellList[_currentSpellIndex];
             _manaCost = _currentSelectedSpell.Cost;
 
-            if (_playerMana >= _manaCost)
+            if (_currentMana >= _manaCost)
             {
                 // Prevent using spells and duplicating lifesteal spell while it's already active
                 if (!DataManager.Instance.IsLifeStealActive)
                 {
-                    _playerMana -= _manaCost;
+                    _currentMana -= _manaCost;
                     _currentSelectedSpell.Cast();
                     SoundManager.Instance.PlaySound2D("Cast");
-                    Debug.Log("Current mana: " + _playerMana);    
+                    Debug.Log("Current mana: " + _currentMana);    
                 }
             }
         }
@@ -77,6 +83,31 @@ namespace Player
 
             Debug.Log("Using: " + _spellList[_currentSpellIndex]);
             SoundManager.Instance.PlaySound2D("NextSpell");
+        }
+
+        // Check if mana is less than max, then regenerate
+        private void Update()
+        {
+            if (_currentMana < _maxMana && !_manaRegen)
+            {
+                _manaRegen = true;
+                StartCoroutine(RegenMana());
+            }
+        }
+
+        // Regenerate mana, don't let mana get over max, wait 1 second
+        private IEnumerator RegenMana()
+        {
+            _currentMana += _manaRegenPerSecond;
+
+            if (_currentMana > _maxMana)
+            {
+                _currentMana = _maxMana;
+            }
+
+            yield return new WaitForSeconds(1);
+            _manaRegen = false;
+            Debug.Log("Current mana: " + _currentMana);
         }
     }
 }
