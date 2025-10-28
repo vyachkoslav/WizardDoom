11c319da0002ea88cd86847c6b35b7311a8413c9
heikki-heinonen <heikki.heinonen@edu.turkuamk.fi>
2025-10-28 16:43:41 +0200

    Damage flash script added
    - object flashes red when it takes damage

---
+++ Assets/Scripts/DamageFlash.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+public class DamageFlash : MonoBehaviour
+{
+    [SerializeField] private Renderer objectRenderer;
+    [SerializeField] private Color flashColor = Color.red;
+    [SerializeField] private float flashDuration = 0.1f;
+
+    private Color originalColor;
+
+    void Start()
+    {
+        originalColor = objectRenderer.material.color;
+    }
+
+    private IEnumerator DoFlash()
+    {
+        objectRenderer.material.color = flashColor;
+        yield return new WaitForSeconds(flashDuration);
+        objectRenderer.material.color = originalColor;
+    }
+
+    public void Flash()
+    {
+        StopAllCoroutines();
+        StartCoroutine(DoFlash());
+    }
+}
