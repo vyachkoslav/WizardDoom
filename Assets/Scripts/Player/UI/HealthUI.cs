22bf0668d5b0162a565b8febb51b647566f89b3f
tyomies123 <46247024+tyomies123@users.noreply.github.com>
2025-10-29 16:15:17 +0200

    Added heal flash when player heals

--- Assets/Scripts/Player/UI/HealthUI.cs
+++ Assets/Scripts/Player/UI/HealthUI.cs
@@ -6,12 +6,18 @@ namespace Player.UI
     public class HealthUI : MonoBehaviour
     {
         [SerializeField] private Slider healthSlider;
-        [SerializeField] private Image damageFlash;
-        [SerializeField] private float flashDuration;
-        [SerializeField] private float flashSpeed;
 
-        private float flashDurationTimer;
-        private float maxAlpha;
+        [Header("Damage and heal visuals")]
+        [SerializeField] private Image damageFlash;
+        [SerializeField] private Image healFlash;
+        [SerializeField] private float flashDuration;
+        [SerializeField] private float fadeSpeed;
+
+        private float damageFlashDurationTimer;
+        private float damageMaxAlpha;
+
+        private float healFlashDurationTimer;
+        private float healmaxAlpha;
 
         private void Start()
         {
@@ -23,22 +29,39 @@ namespace Player.UI
 
             healthSlider.value = PlayerEntity.Instance.Health;
 
-            maxAlpha = damageFlash.color.a;
+            // Store max alpha value and then set damage and heal flash invisible
+            damageMaxAlpha = damageFlash.color.a;
             damageFlash.color = new Color(damageFlash.color.r, damageFlash.color.g, damageFlash.color.b, 0);
+
+            healmaxAlpha = healFlash.color.a;
+            healFlash.color = new Color(healFlash.color.r, healFlash.color.g, healFlash.color.b, 0);
         }
 
-        // If damageFlash is visible, start fading after flash duration
         private void Update()
         {
+            // If damageFlash is visible, start fading after flash duration
             if (damageFlash.color.a > 0)
             {
-                flashDurationTimer += Time.deltaTime;
+                damageFlashDurationTimer += Time.deltaTime;
 
-                if (flashDurationTimer >= flashDuration)
+                if (damageFlashDurationTimer >= flashDuration)
                 {
                     float newAlpha = damageFlash.color.a;
-                    newAlpha -= flashSpeed * Time.deltaTime;
+                    newAlpha -= fadeSpeed * Time.deltaTime;
                     damageFlash.color = new Color(damageFlash.color.r, damageFlash.color.g, damageFlash.color.b, newAlpha);
+                }
+            }
+
+            // If heal flash is visible, start fading after duration
+            if (healFlash.color.a > 0)
+            {
+                healFlashDurationTimer += Time.deltaTime;
+
+                if (healFlashDurationTimer >= flashDuration)
+                {
+                    float newAlpha = healFlash.color.a;
+                    newAlpha -= fadeSpeed * Time.deltaTime;
+                    healFlash.color = new Color(healFlash.color.r, healFlash.color.g, healFlash.color.b, newAlpha);
                 }
             }
         }
@@ -48,14 +71,25 @@ namespace Player.UI
             healthSlider.value = PlayerEntity.Instance.Health;
 
             // damageFlash visible, reset duration timer for Update()
-            damageFlash.color = new Color(damageFlash.color.r, damageFlash.color.g, damageFlash.color.b, maxAlpha);
-            flashDurationTimer = 0;
+            damageFlash.color = new Color(damageFlash.color.r, damageFlash.color.g, damageFlash.color.b, damageMaxAlpha);
+            damageFlashDurationTimer = 0;
 
+            // Make heal flash invisible
+            healFlash.color = new Color(healFlash.color.r, healFlash.color.g, healFlash.color.b, 0);
+            healFlashDurationTimer = 0;
         }
 
         private void OnHealthIncreased()
         {
             healthSlider.value = PlayerEntity.Instance.Health;
+
+            // Heal flash visible, reset timer
+            healFlash.color = new Color(healFlash.color.r, healFlash.color.g, healFlash.color.b, healmaxAlpha);
+            healFlashDurationTimer = 0;
+
+            // Make damage flash invisible
+            damageFlash.color = new Color(damageFlash.color.r, damageFlash.color.g, damageFlash.color.b, 0);
+            damageFlashDurationTimer = 0;
         }
     }
 }
