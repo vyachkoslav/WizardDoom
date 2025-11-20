c808b2936cc8b741fd44e4b0afd909078a12f537
tyomies123 <46247024+tyomies123@users.noreply.github.com>
2025-11-20 18:31:37 +0200

    Added collected item display grid to HUD
    - TODO: change placeholder icon

--- Assets/Scripts/Pickups/Key.cs
+++ Assets/Scripts/Pickups/Key.cs
@@ -1,9 +1,18 @@
+using Player.UI;
 using UnityEngine;
 
 public class Key : Pickup
 {
+    private ItemsUI _itemsDisplay; 
+
+    private void Start()
+    {
+        _itemsDisplay = FindAnyObjectByType<ItemsUI>();
+    }
+
     protected override void PickupEffect()
     {
         DataManager.Instance.AddKeyToList(this);
+        _itemsDisplay.UpdateItemDisplay(this);
     }
 }
---
+++ Assets/Scripts/Player/UI/ItemsUI.cs
@@ -0,0 +1,20 @@
+using UnityEngine.UI;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Player.UI
+{
+    public class ItemsUI : MonoBehaviour
+    {
+        [SerializeField] private LayoutGroup _parent;
+        [SerializeField] private Image _itemDisplay;
+
+        public void UpdateItemDisplay(Key key)
+        {
+            if (DataManager.Instance.KeyList.Contains(key))
+            {
+                Instantiate(_itemDisplay, _parent.transform);
+            }
+        }
+    }
+}
