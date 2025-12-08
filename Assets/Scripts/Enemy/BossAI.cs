51c74278b1f12531f122a0e2c1beb2a948cf2e23
heikki-heinonen <heikki.heinonen@edu.turkuamk.fi>
2025-12-08 15:05:31 +0200

    Boss layer & time freeze (squashed)
    commit 475a92fc35a17455858b98811ef442b92d4d4c79
    Author: heikki-heinonen <heikki.heinonen@edu.turkuamk.fi>
    Date:   Mon Dec 8 14:56:15 2025 +0200
    
        Boss layer & time freeze
    
        - placed boss game objects on Entity layer
        - added enemy time freeze script to bosses
        - added animator disabling to enemy time freeze script

--- Assets/Scripts/Enemy/BossAI.cs
+++ Assets/Scripts/Enemy/BossAI.cs
@@ -43,7 +43,6 @@ public class BossAI : BaseEnemyAI
 
     protected void OnDisable()
     {
-        attacker.OnBeforeAttackDelay -= StartAttackAnimation;
         if (!IsAttacking) return;
 
         OnEndAttacking();
--- Assets/Scripts/Enemy/EnemyTimefreeze.cs
+++ Assets/Scripts/Enemy/EnemyTimefreeze.cs
@@ -7,12 +7,14 @@ public class EnemyTimefreeze : MonoBehaviour
     private BaseEnemyAI _enemyAI;
     private DetectPlayer _detection;
     private NavMeshAgent _navMeshAgent;
+    private Animator _animator;
 
     private void Start()
     {
         _enemyAI = GetComponent<BaseEnemyAI>();
         _detection = GetComponent<DetectPlayer>();
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _animator = transform.Find("Model").GetComponent<Animator>();
     }
 
     public void ApplyTimefreeze(float duration)
@@ -25,12 +27,14 @@ public class EnemyTimefreeze : MonoBehaviour
         _enemyAI.enabled = false;
         _detection.enabled = false;
         _navMeshAgent.enabled = false;
+        _animator.enabled = false;
 
         yield return new WaitForSeconds(duration);
         
         _enemyAI.enabled = true;
         _detection.enabled = true;
         _navMeshAgent.enabled = true;
+        _animator.enabled = true;
     }
 
 }
